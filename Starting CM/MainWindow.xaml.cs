ac5930969d17172a60ec01c2c9764049174883c5
Christopher Miller <chrdmill@ut.utm.edu>
2020-01-30 16:05:25 -0600

    Completed jank paint program

--- Starting CM/MainWindow.xaml.cs
+++ Starting CM/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -32,22 +33,40 @@ namespace Starting_CM
 
         private void Red_Click(object sender, RoutedEventArgs e)
         {
-            Steve.Strokes.Clear();
+            Steve.DefaultDrawingAttributes.Color = Colors.Red;
         }
 
         private void Blue_Click(object sender, RoutedEventArgs e)
         {
-            
+            Steve.DefaultDrawingAttributes.Color = Colors.Blue;
         }
 
         private void Yellow_Click(object sender, RoutedEventArgs e)
         {
-
+            Steve.DefaultDrawingAttributes.Color = Colors.Yellow;
         }
 
         private void Black_Click(object sender, RoutedEventArgs e)
         {
+            Steve.DefaultDrawingAttributes.Color = Colors.Black;
+        }
 
+        private void Small_Click(object sender, RoutedEventArgs e)
+        {
+            Steve.DefaultDrawingAttributes.Width = 2;
+            Steve.DefaultDrawingAttributes.Height = 2;
+        }
+
+        private void Medium_Click(object sender, RoutedEventArgs e)
+        {
+            Steve.DefaultDrawingAttributes.Width = 5;
+            Steve.DefaultDrawingAttributes.Height = 5;
+        }
+
+        private void Large_Click(object sender, RoutedEventArgs e)
+        {
+            Steve.DefaultDrawingAttributes.Width = 10;
+            Steve.DefaultDrawingAttributes.Height = 10;
         }
     }
 }
